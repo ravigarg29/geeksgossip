607184d0a2ad82f8277dad18867cd83cb5bf7b5d
ravigarg29 <ravigarg29@gmail.com>
2015-03-08 06:13:21 +0530

    Revert "connect change to localhost"
    This reverts commit af0d480c056a166f4ffed6474dddd75dd5d2362d.

--- scripts/connect.php
+++ scripts/connect.php
@@ -1,5 +1,5 @@
 <?php
-mysql_connect("localhost","root","password") or die("Could not connect to server");
+mysql_connect("50.62.209.2","Ravi_Bhushan","Anshu#1992") or die("Could not connect to server");
 mysql_select_db("membersip") or die("Could not select database");
 
 ?>
